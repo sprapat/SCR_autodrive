38d74925fef034ce52298c4698d43b102ebee706
Prapat Suriyaphol <prapatsuriyaphol@gmail.com>
2022-03-17 18:25:12 +0700

    add note in get_color for improvement

--- get_color.py
+++ get_color.py
@@ -2,6 +2,12 @@ import numpy as np
 import pyautogui
 import cv2
 
+# The following link may provide a faster solution
+# https://stackoverflow.com/questions/3800458/quickly-getting-the-color-of-some-pixels-on-the-screen-in-python-on-windows-7
+
+# Because the current solution, we do this in many steps
+# capture screen -> convert into numpy array -> convert into cvtColor
+
 def get_color(mon,lower):
         image = pyautogui.screenshot()
         image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
