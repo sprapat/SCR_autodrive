0c8270abfe1b1011c9413c60a947ed9c09e49010
Dew2118 <kavin2118@gmail.com>
2022-03-25 11:55:58 +0700

    remove unused file

--- take_screenshot.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import pyautogui
-import numpy as np
-# thresh = 50
-mon = [970, 20, 950, 30]
-cropped_new_image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)[mon[0]:mon[0]+mon[1], mon[2]:mon[2]+mon[3]]
-# new_bw_image = cv2.threshold(cropped_new_image, thresh, 255, cv2.THRESH_BINARY)[1]
-cv2.imwrite('80.png',cropped_new_image)
